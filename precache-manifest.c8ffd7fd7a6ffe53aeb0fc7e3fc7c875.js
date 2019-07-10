b45f502cffe20a2e5711ddf64a79d940e1cc0f8f
GenesisMauries <exodo_54@hotmail.com>
2019-07-10 13:33:54 -0500

    Updates

---
+++ precache-manifest.c8ffd7fd7a6ffe53aeb0fc7e3fc7c875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce807553da17eb65e63916d8c9d9a38",
+    "url": "/we-love/index.html"
+  },
+  {
+    "revision": "d7047af375df82538664",
+    "url": "/we-love/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/we-love/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d7047af375df82538664",
+    "url": "/we-love/static/js/main.c92a610a.chunk.js"
+  },
+  {
+    "revision": "7c46327bd885d3cbd364",
+    "url": "/we-love/static/js/runtime~main.c4c6b6d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/we-love/static/media/logo.5d5d9eef.svg"
+  }
+]);
